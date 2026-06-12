1d407ac306a39c24053e61a2e69fc35ef4c565f4
kirinonakar <kirinonakar@gmail.com>
2026-06-12 11:47:46 +0900

    verfix

--- Uviewer/AboutDialog.xaml.cs
+++ Uviewer/AboutDialog.xaml.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Reflection;
+using Windows.ApplicationModel;
 
 namespace Uviewer
 {
@@ -12,10 +13,21 @@ namespace Uviewer
         {
             this.InitializeComponent();
             
-            var assembly = typeof(AboutDialog).Assembly;
-            var version = assembly.GetName().Version;
-            var versionString = version != null ? $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}" : "0.0.0.0";
-            VersionText = $"Uviewer ({versionString})";
+            VersionText = $"Uviewer ({GetVersionString()})";
+        }
+
+        private static string GetVersionString()
+        {
+            try
+            {
+                var version = Package.Current.Id.Version;
+                return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+            }
+            catch
+            {
+                var version = typeof(AboutDialog).Assembly.GetName().Version;
+                return version != null ? $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}" : "0.0.0.0";
+            }
         }
 
         private void CloseButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
